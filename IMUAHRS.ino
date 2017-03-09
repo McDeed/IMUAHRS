45d760fa2e1559c91dfc0c951b7f378037dfdbc4
Sigurd Rodahl <Sigurd Rodahl>
2017-03-09 18:13:23 +0100

    First commit

---
+++ IMUAHRS.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  //I2C opsett
+  //RS485 opsett
+  //Qduino opsett
+  //
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //behandle rådata til roll pitch & yaw
+  //bruke trigonometri for å kalkulere helningsvinkel
+  // muligens switchcase for å finne retning på helningsvinkelen
+  //sende retning og helningsvinkel ut fra top-end Qduino
+
+}
